6db2e136acd74efd822c57d82dd1ca7771db438d
gitdeepaks <deepaksankhyan92@outlook.com>
2024-01-17 17:27:46 +0530

    added course creation api 🚀

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -39,6 +39,7 @@ const CreatePage = () => {
     try {
       const res = await axios.post("/api/courses", values);
       router.push(`/teacher/courses/${res.data.id}`);
+      toast.success("Course created");
     } catch (error) {
       toast.error("Something went wrong");
     }
