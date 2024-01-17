06bbf4410e6ae2bf13e64289c83cfdd44ec8ddb6
gitdeepaks <deepaksankhyan92@outlook.com>
2024-01-17 21:39:58 +0530

    partially added course description form 🚀

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <Toaster />
+          <Toaster position="top-center" />
           {children}
         </body>
       </html>
