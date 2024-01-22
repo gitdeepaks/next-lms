5f57f20c785131f2991d7ea5aa3b3dca5a63a55d
gitdeepaks <deepaksankhyan92@outlook.com>
2024-01-22 22:17:17 +0530

    added course attchement form 🚀

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attchment-form.tsx
@@ -0,0 +1,134 @@
+"use client";
+import * as z from "zod";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import {
+  Divide,
+  File,
+  ImageIcon,
+  Loader2,
+  Pencil,
+  Plus,
+  PlusCircle,
+  X,
+} from "lucide-react";
+import { Attachment, Course } from "@prisma/client";
+import Image from "next/image";
+import { FileUpload } from "@/components/file-upload";
+
+interface AttachmentFormProps {
+  initialData: Course & { attachments: Attachment[] };
+  courseId: string;
+}
+
+const formSchema = z.object({
+  url: z.string().min(1),
+});
+
+export const AttachmentForm = ({
+  initialData,
+  courseId,
+}: AttachmentFormProps) => {
+  const [deletingId, setDeletingId] = useState<string | null>(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const router = useRouter();
+
+  const toggleEditing = () => {
+    setIsEditing((prev) => !prev);
+  };
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.post(`/api/courses/${courseId}/attachments`, values);
+      toast.success("Course updated");
+      toggleEditing();
+      router.refresh();
+    } catch (error: any) {
+      toast.error("Something went wrong", error);
+    }
+  };
+
+  const onDelete = async (id: string) => {
+    try {
+      setDeletingId(id);
+      await axios.delete(`/api/courses/${courseId}/attachments/${id}`);
+      toast.success("Course Attachment deleted");
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setDeletingId(null);
+    }
+  };
+
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course Attachments
+        <Button onClick={toggleEditing} variant="ghost">
+          {isEditing && <>Cancel</>}
+          {!isEditing && (
+            <>
+              <PlusCircle className="h-4 w-4 mr-2" />
+              Add a file
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <>
+          {initialData.attachments.length === 0 && (
+            <p className="text-sm mt-2 text-slate-500 italic">
+              No attachments yet.
+            </p>
+          )}
+          {initialData.attachments.length > 0 && (
+            <div className="space-y-2">
+              {initialData.attachments.map((attachment) => (
+                <div
+                  key={attachment.id}
+                  className="flex items-center p-3 w-full bg-sky-100 border-sky-200 border text-sky-700 rounded-md"
+                >
+                  <File className="h-4 w-4 mr-2 flex-shrink-0" />
+                  <p className="text-xs line-clamp-1">{attachment.name}</p>
+                  {deletingId === attachment.id && (
+                    <div>
+                      <Loader2 className="h-4 w-4 animate-spin" />
+                    </div>
+                  )}
+                  {deletingId !== attachment.id && (
+                    <Button
+                      onClick={() => onDelete(attachment.id)}
+                      variant="ghost"
+                      className="ml-auto hover:opacity-75 transition"
+                    >
+                      <X className="h-4 w-4" />
+                    </Button>
+                  )}
+                </div>
+              ))}
+            </div>
+          )}
+        </>
+      )}
+      {isEditing && (
+        <div>
+          <FileUpload
+            endpoint="courseAttachement"
+            onChange={(url) => {
+              if (url) {
+                onSubmit({ url: url });
+              }
+            }}
+          />
+          <div className="text-xs text-muted-foreground mt-4">
+            Add anything your students need to complete the course.
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,12 +4,18 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { boolean } from "zod";
 import { IconBadge } from "@/components/icon-badge";
-import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
+import {
+  CircleDollarSign,
+  File,
+  LayoutDashboard,
+  ListChecks,
+} from "lucide-react";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
+import { AttachmentForm } from "./_components/attchment-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -20,6 +26,13 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const course = await db.course.findUnique({
     where: {
       id: params.courseId,
+    },
+    include: {
+      attachments: {
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
     },
   });
 
@@ -90,6 +103,13 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
               <h2 className="text-xl">Sell Your Course</h2>
             </div>
             <PriceForm initialData={course} courseId={course.id} />
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={File} />
+              <h2 className="text-xl">Resources and Attachments</h2>
+            </div>
+            <AttachmentForm initialData={course} courseId={course.id} />
           </div>
         </div>
       </div>
