107dbbaa095265092546752d2fd91e4dfcffc419
gitdeepaks <deepaksankhyan92@outlook.com>
2024-01-25 22:34:04 +0530

    added editor and previewquill🚀

---
+++ components/preview.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+import "react/quill/dist/quill.bubble.css";
+
+interface PerviewProps {
+  value: string;
+}
+
+export const Preview = ({ value }: PerviewProps) => {
+  const ReactQuill = useMemo(
+    () => dynamic(() => import("react-quill"), { ssr: false }),
+    []
+  );
+
+  return (
+    <div className="bg-white">
+      <ReactQuill theme="bubble" value={value} readOnly />
+    </div>
+  );
+};
