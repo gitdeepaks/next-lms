5af465fda2fb6ebac0a4a1c2b9b6735ba8ba9538
gitdeepaks <deepaksankhyan92@outlook.com>
2024-01-27 21:08:01 +0530

    added data table🚀

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,31 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId: userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
